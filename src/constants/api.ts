e0c86a76db1bf0ea4c773421f4269a7fbf67467f
amir-h <a.h.mohammadi5@gmail.com>
2026-09-18 13:26:21 +0330

    change api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://backend.fitlineclinic.com";
 
 export const API_URL = {
   Doctors: `${API_BASE_URL}/api/doctors`,
